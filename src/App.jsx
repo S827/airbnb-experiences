60f532a6dad4bb1c286bd26ca1653b4cd2cd2497
s827 <dskumar827@gmail.com>
2022-10-31 12:10:38 +0530

    started latest react project added navbar and its styles

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Navbar from './components/navbar'
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
