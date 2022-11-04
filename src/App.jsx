5f5e235939ef63e7598e913cd81963f3e7497323
s827 <dskumar827@gmail.com>
2022-11-04 21:52:57 +0530

    added SOLD OUT and ONLINE badge

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ export default function App() {
         reviewCount={item.stats.reviewCount}
         location={item.location}
         price={item.price}
+        openSpots={item.openSpots}
       />
     })
   return (
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 import Star from '../images/star.png'
 export default function Card(props) {
+    let badgeText
+    if(props.openSpots===0){
+        badgeText="SOLD OUT"
+    } else if(props.location === 'Online') {
+        badgeText = 'ONLINE'
+    }
     return (
         <div className='card'>
+            {badgeText && <div className='card-badge'>{badgeText}</div>}
             <img className='card-image' src={`images/${props.img}`} />
             <div className='card-stats'>
                 <img src={Star} className='card-star' />
